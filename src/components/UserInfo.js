6b9a1a8348d019ae0af5e6caf96cf55ea1f91498
jessicaquasar <jessica.quasar@gmail.com>
2019-04-14 23:10:36 -0300

    Indentations fix

--- src/components/UserInfo.js
+++ src/components/UserInfo.js
@@ -7,19 +7,19 @@ function UserInfo(props) {
   const userInfo = props.user ?
     (
       <div className='card'>
-				<img className='card-img-top' src={ props.user.avatar_url } alt='avatar'/>
-				<div className='card-body'>
-					<h5 className='card-title'>{ props.user.login }</h5>
-					<p className='card-text'>
-						Name: { props.user.name } <br />
-          	Followers: { props.user.followers } / Following: { props.user.following }
-					</p>
-					<a href={ props.user.html_url } className='btn btn-primary' target='blank' >View details</a>
-				</div>
+        <img className='card-img-top' src={props.user.avatar_url} alt='avatar' />
+        <div className='card-body'>
+          <h5 className='card-title'>{props.user.login}</h5>
+          <p className='card-text'>
+            Name: {props.user.name} <br />
+            Followers: {props.user.followers} / Following: {props.user.following}
+          </p>
+          <a href={props.user.html_url} className='btn btn-primary' target='blank' >View details</a>
+        </div>
       </div>
     ) : null;
 
-    return userInfo;
+  return userInfo;
 }
 
 UserInfo.propTypes = {
